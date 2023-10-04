449d0729c6ab76c5467feef52cf9ebb6e3ceb93f
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-10-04 23:32:08 +1100

    added portfolio page

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 
-const Project = () => {
+const Project = ({ title, description }) => {
   return (
     <div className="project">
       <img
         src="https://via.placeholder.com/300x200" 
         alt="Project Image"
       />
-      <h3>Project Title</h3>
-      <p>Project Description goes here.</p>
-      <ul>
-        <li>Feature 1</li>
-        <li>Feature 2</li>
-        <li>Feature 3</li>
-      </ul>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      
       <a href="#" target="_blank" rel="noopener noreferrer">
         View Project
       </a>
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Project from "../../components/Project/Project"; 
+
+const Portfolio = () => {
+  const projectsData = [
+    { id: 1, title: "Project 1", description: "Description 1" },
+    { id: 2, title: "Project 2", description: "Description 2" },
+    { id: 3, title: "Project 3", description: "Description 3" },
+    { id: 4, title: "Project 4", description: "Description 4" },
+    { id: 5, title: "Project 5", description: "Description 5" },
+    { id: 6, title: "Project 6", description: "Description 6" },
+  ];
+
+  return (
+    <div className="container">
+      <div className="row">
+        {projectsData.map((project) => (
+          <div key={project.id} className="col-lg-6">
+            <Project title={project.title} description={project.description} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
