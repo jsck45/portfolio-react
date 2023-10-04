688281442844745c0909bc0e335f244ba0146773
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-10-04 23:21:18 +1100

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-// import Contact from './pages/Contact/Contact';
+import Contact from './pages/Contact/Contact';
 // import Portfolio from './pages/Portfolio/Portfolio';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -15,7 +15,7 @@ function App() {
 
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/contact" element={<Contact />} />
         {/* <Route path="/portfolio" element={<Portfolio />} /> */}
 
       </Routes>
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = {};
+    if (!formData.name.trim()) {
+      validationErrors.name = "Name is required";
+    }
+    if (!formData.email.trim()) {
+      validationErrors.email = "Email is required";
+    } else if (!isValidEmail(formData.email)) {
+      validationErrors.email = "Invalid email address";
+    }
+    if (!formData.message.trim()) {
+      validationErrors.message = "Message is required";
+    }
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      console.log("Form data submitted:", formData);
+      setFormData({ name: "", email: "", message: "" });
+      setErrors({});
+    }
+  };
+
+  const isValidEmail = (email) => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  };
+
+  return (
+    <div className="contact-container">
+      <h1>Contact Me</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          {errors.name && <p className="error">{errors.name}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            required
+          />
+          {errors.message && <p className="error">{errors.message}</p>}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
