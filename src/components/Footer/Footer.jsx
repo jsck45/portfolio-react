2674e2b9052dbda5bdc5943681218578aee9a063
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-10-05 16:13:17 +1100

    icon hover footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer menu_footer d-flex flex-column flex-md-row justify-content-md-center align-items-md-center w-100 p-3">
       <div className="d-flex">
-        <a href="https://linkedin.com/in/carol-js-kim" target="_blank" rel="noopener noreferrer" className="mx-2">LinkedIn</a>
-        <a href="https://github.com/jsck45" target="_blank" rel="noopener noreferrer" className="mx-2">Github</a>
-        <a href="https://stackoverflow.com/users/22682012/carol-kim" target="_blank" rel="noopener noreferrer" className="mx-2">Stack Overflow</a>
+        <a
+          href="https://linkedin.com/in/carol-js-kim"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-2"
+        >
+          <i className="fa fa-linkedin icon"></i> 
+        </a>
+        <a
+          href="https://github.com/jsck45"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-2"
+        >
+          <i className="fa fa-github icon"></i> 
+        </a>
+        <a
+          href="https://stackoverflow.com/users/22682012/carol-kim"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-2"
+        >
+          <i className="fa fa-stack-overflow icon"></i> 
+        </a>
       </div>
     </div>
   );
