2fb5357ed6e0c4dfb79617c6ef27cc4c3e5739be
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-10-05 16:22:11 +1100

    typewriter header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
         <div className="intro-section">
-          <h1 className="intro-title">Hi, I'm Carol</h1>
-          <p className="intro-description">Your description goes here.</p>
+          <h1 className="intro-title" >
+            Hi, I'm Carol
+          </h1>
+          <br />
+          <div className="typewriter-container">
+            <Typewriter
+              options={{
+                strings: ["Web Developer", "React Enthusiast", "Lifelong Learner"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
         </div>
       </div>
     </header>
