17f9f0ce6a5f9d0c7b6d6ee9d4087e307102d8b0
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-10-07 20:19:55 +1100

    moved _redirects into public

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "../../components/Header/Header";
 import Contact from "../../pages/Contact/Contact";
 import PortfolioScroll from "../Portfolio/PortfolioScroll";
